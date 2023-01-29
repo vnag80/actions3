a0be1ece9149878aab03d51c6a2b2db3a75f7918
vnag80 <VNAG80@gmail.com>
2023-01-29 15:11:12 -0500

    changes

--- .github/Actions/deploy-s3-javascript/main.js
+++ .github/Actions/deploy-s3-javascript/main.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 function run()
 {
   core.notice("Hurray we are in javascript actions")
@@ -8,6 +9,12 @@ function run()
   core.notice(bucket_nm);
   core.notice(region_nm);
   core.notice(path_nm)
+  bucket_uri = `s3://${bucket_nm}`
+  core.notice(bucket_uri);
+  exec.exec(`aws s3 sync ${path_nm} ${bucket_uri} --region ${region_nm}`)
+  const webURL = `http://${bucket_nm}.s3-website-${region_nm}.amazonaws.com`
+  core.setOutput('web-url',webURL)
+
 
 
 }
