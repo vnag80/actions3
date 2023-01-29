03ce1c30fc399b113773010c4de7b96ff49dc96b
vnag80 <VNAG80@gmail.com>
2023-01-29 14:39:19 -0500

    added inputs

--- .github/Actions/deploy-s3-javascript/main.js
+++ .github/Actions/deploy-s3-javascript/main.js
@@ -2,5 +2,13 @@ const core = require('@actions/core');
 function run()
 {
   core.notice("Hurray we are in javascript actions")
+  const bucket_nm = core.getInput('bucket-name');
+  const region_nm = core.getInput('aws-region');
+  const path_nm = core.getInput('dist-file-path')
+  core.notice(bucket_nm);
+  core.notice(region_nm);
+  core.notice(path_nm)
+
+
 }
 run();
